a505cbebe1aa4f5d5ec539414aa572a095532f5b
samriddhsingh123 <samriddh20466@iiitd.ac.in>
2021-12-21 15:01:02 +0530

    gravity maintained

--- WillHeroGame/src/sample/Controller2.java
+++ WillHeroGame/src/sample/Controller2.java
@@ -20,33 +20,23 @@ public class Controller2 {
     private Button Start;
     @FXML
     private Button closeButton;
-
     public void switchToScene1(ActionEvent event) throws IOException {
         FXMLLoader ld=new FXMLLoader(getClass().getResource("sample.fxml"));
         root=ld.load();
         Controller cont=ld.getController();
         cont.switchToGame(event);
     }
-
+    @FXML
+    public void handleCloseButtonAction(ActionEvent event) {
+        this.stage = (Stage)this.closeButton.getScene().getWindow();
+        this.stage.close();
+    }
 
     public void exitToMainMenu(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root,1100,800);
         stage.setTitle("Will Hero");
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    public void handleCloseButtonAction(ActionEvent event) {
-        stage = (Stage) closeButton.getScene().getWindow();
-
-        stage.close();
-    }
-    public void GoToExit(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("exit.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
@@ -58,12 +48,18 @@ public class Controller2 {
     }
 
     public void OpenCredit(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("CreditScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/CreditScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setTitle("Will Hero");
         scene = new Scene(root,1100,800);
         stage.setScene(scene);
         stage.show();
     }
-
+    public void GoToExit(ActionEvent event) throws IOException {
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("/sample/exit.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root,1100,800);
+        this.stage.setScene(this.scene);
+        this.stage.show();
+    }
 }
